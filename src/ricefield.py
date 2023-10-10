b934cf66003d29a2e898433d470ec31281fa2b84
SikesJohn <113187445+SikesJohn@users.noreply.github.com>
2023-10-11 02:13:40 +0800

    added initial static views for flask webpages
    Homepage, categories, subcategories pages , non responsive for now

--- src/ricefield.py
+++ src/ricefield.py
@@ -2,10 +2,25 @@ from flask import *
 app = Flask(__name__, template_folder='template')
 
 
-@app.route('/')
-def message():
+@app.route('/',methods=['GET','POST'])
+def homepage():
     return render_template('index.html')
 
+@app.route('/subcategory', methods=["GET","POST"])
+def subcategory():
+    return render_template('subcategory.html',categoryName="beauty-placeholder")
+
+@app.route('/category', methods=["GET","POST"])
+def category():
+    return render_template('category.html', categoryName="category-placeholder")
+
+@app.route('/categories', methods=["GET","POST"])
+def categories():
+    return render_template('categories.html')
+
+@app.route('/subcategories', methods=["GET","POST"])
+def subcategories():
+    return render_template('subcategories.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
