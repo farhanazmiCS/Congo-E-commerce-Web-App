6c114a171555dd72b131841867d1975270249462
Ernest Foo <113896138+ErnestFoo@users.noreply.github.com>
2023-10-29 00:10:21 +0800

    Fixed quantity more than stock error

--- src/app_pages/cart.py
+++ src/app_pages/cart.py
@@ -7,30 +7,30 @@ from mongodbcontrollerV2 import MongoDBController
 def getCart():
     if 'user_id' in session:
         controller = MongoDBController()
-        cart_cursor = controller.read('Cart', {'user_id': session['user_id']})
+        cart_cursor = controller.read('Cart', {'user_id': session['user_id']})    
         cart = list(cart_cursor)  # Convert the cursor to a list of documents
-        for cart_item in cart:
-            return cart_item  # normally, only 1 cart item in cart, as users can only have 1 cart
+        if cart:
+            return cart[0]
     else:
-        error_message += "User not logged in or an error occurred, "
-        return None
+        session['error_message'] = "You must be logged in to view your cart"
+        return redirect(url_for('login'))
 
 
-def getProducts(cart_item):
+def getProducts(cart):
     if cart is not None:
         product_array = []  # Array of product dictionaries
         # getting total products in cart
-        products = cart_item.get('products', [])
+        products = cart.get('products', [])
         for product in products:  # looping through each product in cart
             # getting product id to perform details query
             product_id = product.get('product_id')
             product_details = getProductDetails(
-                   product_id, product.get('quantity'))  # getting product details
+                product_id, product.get('quantity'))  # getting product details
 
             if product_details is not None:  # if the product exist and is in stock
-                    # product_details['product_quantity'] = product.get('quantity') # adding quantity to product details
-                    # adding product details to array
-                    product_array.append(product_details)
+                # product_details['product_quantity'] = product.get('quantity') # adding quantity to product details
+                # adding product details to array
+                product_array.append(product_details)
         return product_array
 
 
@@ -49,10 +49,12 @@ def getProductDetails(product_id, product_quantity):
     product_name = product[0][1]
 
     if product_stock == 0:
-        error_message += "Product {product_name} has been removed from your cart as it is out of stock, "
+        session['error_message'].append(
+            f"Product {product_name} has been removed from your cart as it is out of stock, ")
         return None
     elif product_stock < product_quantity:
-        error_message += "Product {product_name} has less stock than the quantity in your cart. Quantity has been reduced to available stock, "
+        session['error_message'].append(
+            f"Product {product_name} has less stock than the quantity in your cart. Quantity has been reduced to available stock, ")
         product_quantity = product_stock
 
     if product:
@@ -77,35 +79,30 @@ def getSubtotal(products):
 
 @app.route('/cart', methods=["GET", "POST"])
 def cart():
-    error_message = ""  # Initialize an error message variable
-    # print(session.get('cart'))
+    # Initialize an error message variable
+    session.setdefault('error_message', [])
     if session.get('user_id') is None:
-        error_message += "User not logged in or an error occurred, "
-        session['error_message'] = error_message
+        session['error_message'] = "You must be logged in to view your cart"
         return redirect(url_for('login'))
-    
-    #if the session cart is empty, pull cart from database
-    if session.get('cart') == []:
+
+    print("the user id is: ")
+    print(session.get('user_id'))
+    print(session.get('cart'))
+    # if the session cart is empty, pull cart from database
+    if session.get('cart') is None or session.get('cart') == []:
         cart = getCart()
         products = getProducts(cart)
-        print(products)
         session['cart'] = products
         subtotal = getSubtotal(products)
-        print(session['cart'])
 
-    else: #if the session cart is not empty, use the session cart
+    else:  # if the session cart is not empty, use the session cart
+        # print("session cart not empty")
+        # print(session.get('cart'))
         products = session['cart']
         subtotal = getSubtotal(products)
 
-    error_message += "Banana, "
-    error_message += "Apple, "
-    error_message += "Orange, "
+    session['error_message'].append("Banana, ")
+    session['error_message'].append("Apple, ")
+    session['error_message'].append("Orange, ")
 
-    if error_message:  # Check if there are any error messages
-        # Split the error messages by comma and space
-        error_messages = error_message.split(", ")
-        error_messages.pop()  # Remove the last element from the list as it is an empty string
-    else:
-        error_messages = []
-
-    return render_template('cart.html', products=products, subtotal=subtotal, error_messages = error_messages)
+    return render_template('cart.html', products=products, subtotal=subtotal)
